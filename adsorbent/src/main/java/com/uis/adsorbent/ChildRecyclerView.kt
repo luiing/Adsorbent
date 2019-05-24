90244dbdf2f53af8f9ac89addac9126da06b9c5b
uis <luiing@foxmail.com>
2019-05-24 22:32:54 +0800

    快速滑动联动以速度为基准，加入parent联动child处理

--- adsorbent/src/main/java/com/uis/adsorbent/ChildRecyclerView.kt
+++ adsorbent/src/main/java/com/uis/adsorbent/ChildRecyclerView.kt
@@ -20,43 +20,29 @@ class ChildRecyclerView :RecyclerView{
 
     /** true 开启滑动冲突处理*/
     var enableConflict = true
-    /** 抬手动作记录滚动状态*/
-    private var actionUpState = SCROLL_STATE_IDLE
     /** 记录上次的parent,避免递归频繁*/
     private var parentView :WeakReference<OnInterceptListener>? = null
 
     init {
         addOnScrollListener(object :OnScrollListener(){
             override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-                //Log.e("xx","child statechanged $newState, isTop="+!canScrollVertically(-1))
                 /** 滚动停止且到了顶部,快速滑动事件往上给parent view*/
-                if(SCROLL_STATE_IDLE == newState && !canScrollVertically(-1) && SCROLL_STATE_DRAGGING == actionUpState){
-                    parentView?.get()?.let {
-                        it.onScrollChain()
-                    }
+                if(SCROLL_STATE_IDLE == newState && !canScrollVertically(-1)){
+                    parentView?.get()?.onScrollChain()
                 }
             }
         })
     }
 
     override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
-        /** true child在顶部*/
         if(enableConflict) {
             induceParentOfChildTopStatus()
-            when(ev.action){
-                MotionEvent.ACTION_DOWN ->{
-                    actionUpState = SCROLL_STATE_IDLE
-                }
-                MotionEvent.ACTION_UP -> {
-                    actionUpState = scrollState
-                }
-            }
         }
-        //Log.e("xx","child action ${ev.action} state $scrollState,y= ${ev.y}")
         return super.dispatchTouchEvent(ev)
     }
 
     private fun induceParentOfChildTopStatus(){
+        /** true child在顶部*/
         val isChildTop = !canScrollVertically(-1)
         parentView?.get()?.let {
             it.onTopChild(isChildTop)
